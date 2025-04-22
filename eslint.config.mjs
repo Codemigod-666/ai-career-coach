e8dc7fe6c45bd5f9aa05fed66812e865b0eddec6
Rishi-Inventive <rishi.khandagle@inventivebizsol.com>
2025-04-22 16:28:08 +0000

    resolving the build errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,18 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+// export default eslintConfig;
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -9,6 +24,12 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = {
+  ...compat.extends("next/core-web-vitals"),
+  parserOptions: {
+    ecmaVersion: 2020, // ensure this is set properly
+    sourceType: "module",
+  },
+};
 
 export default eslintConfig;
